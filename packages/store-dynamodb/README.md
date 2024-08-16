f32b19e47af4f5aac6e6d585128fc08e7098a98b
zirkelc <chris.zirkel@gmail.com>
2024-08-16 13:17:19 +0200

    chore: add dynamodb package placeholder

---
+++ packages/store-dynamodb/README.md
@@ -0,0 +1,3 @@
+# Work in Progress
+
+This package is not implemented yet! Please check https://github.com/zirkelc/middy-store/issues/1 for updates.
---
+++ packages/store-dynamodb/src/index.ts
@@ -0,0 +1,3 @@
+console.error(
+	"This package is not implemented yet. Please check https://github.com/zirkelc/middy-store/issues/1 for updates.",
+);
